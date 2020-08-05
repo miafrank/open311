54d816b52b33586f6cb03d078e19c1ab349d3e77
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-05 10:45:56 -0500

    moved schemas

--- load_schemas.py
+++ load_schemas.py
@@ -2,13 +2,13 @@ import json
 
 
 def load_resources():
-    with open('request_schema.json') as request_schema:
+    with open('schemas/request_schema.json') as request_schema:
         request_schema = json.load(request_schema)
 
-    with open('service_schema.json') as service_schema:
+    with open('schemas/service_schema.json') as service_schema:
         service_schema = json.load(service_schema)
 
-    with open('service_definition_schema') as service_definition_schema:
+    with open('schemas/service_definition_schema') as service_definition_schema:
         service_definition_schema = json.load(service_definition_schema)
 
     with open('open311_cities') as open311_cities:
